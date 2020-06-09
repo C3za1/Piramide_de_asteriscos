52f10db16377a111e64905e3941a22344dfcdc07
C3za1 <C3za1.97@gmail.com>
2020-06-09 00:40:31 -0500

    Piramide_de_asteriscos
    Piramide de asteriscos en c++

---
+++ PIRAMIDE.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iostream>
+
+using namespace std; 
+
+main()
+{
+	
+	int i;
+	int n;
+	int valor;
+	
+	cout<<"introdusca valor (1)"<<endl;
+	cin>>valor;
+	cout<<endl;
+	
+	for(i=1; i<=valor; i++){
+	for(n=1; i>=n; n++)
+	 cout<<"?";
+	 cout<<"\n";
+	}
+	
+}
